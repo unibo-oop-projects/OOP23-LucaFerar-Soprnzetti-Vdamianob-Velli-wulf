43612fbf0029487a774a464ec26f57e3c31c752b
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-05 21:23:18 +0100

    built abstract class "Character"

---
+++ src/main/java/javawulf/model/Character.java
@@ -0,0 +1,23 @@
+package javawulf.model;
+
+public abstract class Character extends GameObject {
+    private Integer speed;
+    private BoundingBox hitBox;
+
+    public Integer getSpeed() {
+        return this.speed;
+    }
+
+    public BoundingBox getHitBox() {
+        return this.hitBox;
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
+
+    public void setHitBox(BoundingBox hitBox) {
+        this.hitBox = hitBox;
+    }
+
+}
