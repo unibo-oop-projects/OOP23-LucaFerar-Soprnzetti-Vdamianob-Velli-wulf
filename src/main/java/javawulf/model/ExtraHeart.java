ee06e7ffcef4b136372e1847f0c7f7853dcc76a7
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-06 20:59:44 +0100

    added extra heart

---
+++ src/main/java/javawulf/model/ExtraHeart.java
@@ -0,0 +1,16 @@
+package javawulf.model;
+
+public class ExtraHeart extends Collectable implements Item {
+
+    public ExtraHeart(PositionOnMap position, Integer points) {
+        super(position, points);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void applyEffect() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyEffect'");
+    }
+
+}
