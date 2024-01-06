7d01cbecd4d1406feeb61bf3224ee0df65c336eb
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-06 21:03:51 +0100

    added cure and its stronger counterpart cure max

---
+++ src/main/java/javawulf/model/CureMax.java
@@ -0,0 +1,16 @@
+package javawulf.model;
+
+public class CureMax extends Cure {
+
+    public CureMax(PositionOnMap position, Integer points) {
+        super(position, points);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void applyEffect() {
+        // TODO Auto-generated method stub
+        super.applyEffect();
+    }
+    
+}
