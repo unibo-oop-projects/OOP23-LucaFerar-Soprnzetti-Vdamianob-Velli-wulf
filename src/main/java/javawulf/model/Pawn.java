ac5fd87912c98ce036ba1cf5b38881c2c0191a13
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-06 14:33:05 +0100

    Added Pawn

---
+++ src/main/java/javawulf/model/Pawn.java
@@ -0,0 +1,17 @@
+package javawulf.model;
+
+public class Pawn extends EnemyImpl {
+
+    @Override
+    public void move() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+
+    @Override
+    public void takeHit() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'takeHit'");
+    }
+    
+}
