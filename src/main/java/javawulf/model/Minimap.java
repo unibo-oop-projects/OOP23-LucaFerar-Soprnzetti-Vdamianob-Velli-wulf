96a6c30db359b472eb0563c0fcb377c39adb8abf
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-06 21:00:24 +0100

    added minimap

---
+++ src/main/java/javawulf/model/Minimap.java
@@ -0,0 +1,16 @@
+package javawulf.model;
+
+public class Minimap extends Collectable implements Item  {
+
+    public Minimap(PositionOnMap position, Integer points) {
+        super(position, points);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void applyEffect() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyEffect'");
+    }
+    
+}
