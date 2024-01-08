bf92a729ac5dfff2bb0df3544ae843a3280834f3
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-08 22:34:15 +0100

    Added constructor

--- src/main/java/javawulf/model/Pawn.java
+++ src/main/java/javawulf/model/Pawn.java
@@ -2,6 +2,10 @@ package javawulf.model;
 
 public class Pawn extends EnemyImpl {
 
+    public Pawn(BoundingBox collision, PositionOnMap position, Integer speed, BoundingBox hitBox, Integer points) {
+        super(collision, position, speed, hitBox, points);
+    }
+
     @Override
     public void move() {
         // TODO Auto-generated method stub
