83891feabd217734dd6196789170e1056e1f51a9
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-09 11:38:20 +0100

    Added direction and implemented in Entity

--- src/main/java/javawulf/model/Entity.java
+++ src/main/java/javawulf/model/Entity.java
@@ -3,6 +3,7 @@ package javawulf.model;
 public abstract class Entity extends GameObject {
     private Integer speed;
     private BoundingBox hitBox;
+    private Direction direction;
 
     public Integer getSpeed() {
         return this.speed;
@@ -12,12 +13,20 @@ public abstract class Entity extends GameObject {
         return this.hitBox;
     }
 
+    public Direction getDirection() {
+        return this.direction;
+    }
+
     public void setSpeed(Integer speed) {
         this.speed = speed;
     }
 
     public void setHitBox(BoundingBox hitBox) {
         this.hitBox = hitBox;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 
 }
