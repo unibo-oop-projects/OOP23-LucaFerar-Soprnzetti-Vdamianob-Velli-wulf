b4c5fc95718347e60f6cb73e0cc1e3b9ff5c11a9
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-16 13:50:25 +0100

    Added methods to the Player interface

--- src/main/java/javawulf/model/Player.java
+++ src/main/java/javawulf/model/Player.java
@@ -2,4 +2,19 @@ package javawulf.model;
 
 public interface Player {
     
+    public void attack();
+
+    public void move() throws IllegalStateException;
+
+    public boolean isHit();
+
+    public void changeSwordType();
+
+    public boolean isAmuletPieceInCoordinate();
+
+    public int getHealth();
+
+    public void usePowerUp(PowerUp p);
+
+    public boolean isDefeated();
 }
