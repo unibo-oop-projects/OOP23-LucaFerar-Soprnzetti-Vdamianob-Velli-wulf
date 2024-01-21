94585a7c07ab8f67e167cef9499cec75336eeb9f
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-21 19:01:40 +0100

    first raw implementation of guard

--- src/main/java/javawulf/model/Guard.java
+++ src/main/java/javawulf/model/Guard.java
@@ -1,37 +1,59 @@
 package javawulf.model;
 
+import javawulf.model.PlayerImpl.SwordType;
+
 public class Guard extends EnemyImpl {
 
-    private Integer killValue;
+    private final SwordType killValue;
+    private boolean isAlive;
+    private boolean isStunned;
+    private long stunTime;
 
     public Guard(BoundingBox collision, PositionOnMap position, Integer speed, BoundingBox hitBox, Integer points,
-            Integer killValue) {
+            SwordType killValue) {
         super(collision, position, speed, hitBox, points);
         this.killValue = killValue;
+        this.isAlive = true;
     }
 
-    public Integer getKillValue() {
-        return this.killValue;
+    public boolean isAlive() {
+        return isAlive;
     }
 
-    public boolean isKillable() {
-        // TODO implement here
-        return false;
+    public void setAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public boolean isKillable(Player p) {
+        return this.killValue.equals(p.getSwordType());
     }
 
     public void checkRoom() {
         // TODO implement here
     }
 
-    @Override
-    public void move() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    public void stun(Integer time) {
+        this.isStunned = true;
+        this.stunTime = System.currentTimeMillis() + time * 1000;
     }
 
     @Override
-    public void takeHit() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'takeHit'");
+    public void move() {
+        if (this.isStunned) {
+            if (System.currentTimeMillis() >= this.stunTime) {
+                this.isStunned = false;
+            }
+        } else {
+            throw new UnsupportedOperationException("Unimplemented method 'move'");
+        }
+    }
+
+    @Override
+    public void takeHit(Player p) {
+        if (this.isKillable(p)) {
+            this.setAlive(false);
+        } else {
+            this.stun(4);
+        }
     }
 }
