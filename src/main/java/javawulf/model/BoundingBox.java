1518c6fab48af5ad527fa26943ca45be67212cd6
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-22 16:07:55 +0100

    Added missing methods to the BoundingBox interface

--- src/main/java/javawulf/model/BoundingBox.java
+++ src/main/java/javawulf/model/BoundingBox.java
@@ -1,6 +1,20 @@
 package javawulf.model;
 
+import java.awt.Rectangle;
+
 public interface BoundingBox {
 
-    public boolean isCollidingWith();
+    public enum CollisionType{
+        PLAYER,
+        ENEMY,
+        COLLECTABLE,
+        SWORD,
+        STUNNED; //could get renamed in the future
+    }
+
+    public boolean isCollidingWith(Rectangle box);
+
+    public CollisionType getCollisionType();
+
+    public void changeCollisionType(CollisionType type);
 }
