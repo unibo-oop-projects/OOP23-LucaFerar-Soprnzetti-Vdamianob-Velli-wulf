149ec26dd48a96b6d03f04526caac28be4cd052c
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-22 20:39:22 +0100

    Removed position attribute and methods (are already on GameObject)

--- src/main/java/javawulf/model/Collectable.java
+++ src/main/java/javawulf/model/Collectable.java
@@ -2,30 +2,16 @@ package javawulf.model;
 
 public abstract class Collectable extends GameObject {
 
-    private PositionOnMap position;
     private Integer points;
-
-    public Collectable(PositionOnMap position, Integer points) {
-        this.position = position;
-        this.points = points;
-    }
-
+    
     public Integer getPoints() {
         return this.points;
-    }
-
-    public PositionOnMap getPosition() {
-        return this.position;
     }
 
     public void setPoints(Integer points) {
         this.points = points;
     }
-
-    public void setPosition(PositionOnMap position) {
-        this.position = position;
-    }
-
+  
     public void collect(Player p){
         if (p.getPosition().equals(this.position)) {
             this.applyEffect(p);
