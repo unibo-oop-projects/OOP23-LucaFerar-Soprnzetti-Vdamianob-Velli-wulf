7d1de4cfec15c5d568808bd967ff04e53db5821c
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-22 12:01:19 +0100

    Made first implementation of Sword

---
+++ src/main/java/javawulf/model/SwordImpl.java
@@ -0,0 +1,36 @@
+package javawulf.model;
+
+public class SwordImpl extends GameObject implements Sword {
+
+    private final static int NORMAL = 1;
+    private final static int STRONG = 2;
+    private int strength;
+    private SwordType swordType;
+
+    public SwordImpl(){
+        this.strength = NORMAL;
+        this.swordType = SwordType.GREATSWORD;
+    }
+
+    @Override
+    public int getSwordStrength() {
+        return this.strength;
+    }
+
+    @Override
+    public void setSwordStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public SwordType getSwordType() {
+        return this.swordType;
+    }
+
+    @Override
+    public void changeSwordType() {
+        this.swordType = this.swordType==SwordType.NORMAL ? SwordType.GREATSWORD : SwordType.NORMAL;
+        this.strength = this.swordType==SwordType.GREATSWORD ? STRONG : this.strength;
+    }
+    
+}
