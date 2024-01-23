b795108028204897b5a0a3b8fe25001bd219d7c3
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-23 11:34:41 +0100

    Implemented TileType

--- src/main/java/javawulf/model/map/TileType.java
+++ src/main/java/javawulf/model/map/TileType.java
@@ -1,5 +1,18 @@
 package javawulf.model.map;
 
 public enum TileType {
+    WALL(false),
+    ROOM(true),
+    DOOR(true),
+    CORRIDOR(true);
+    
+    private final boolean crossable;
 
+    TileType(boolean crossable) {
+        this.crossable = crossable;
+    }
+
+    public boolean isCrossable() {
+        return this.crossable;
+    }
 }
