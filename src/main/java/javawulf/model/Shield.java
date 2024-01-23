aa0e6be7d971d576f51f18f9c8d5e3f12f2f7f61
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-23 15:47:23 +0100

    removed useless methods and parameters in Shield (are used in PlayerHealth)

--- src/main/java/javawulf/model/Shield.java
+++ src/main/java/javawulf/model/Shield.java
@@ -4,19 +4,8 @@ import javawulf.model.PlayerHealth.ShieldStatus;
 
 public class Shield extends Collectable implements Item {
 
-    private int duration;
-
-    public Shield(PositionOnMap position, Integer points, Integer duration) {
+    public Shield(PositionOnMap position, Integer points) {
         super(position, points);
-        this.duration = duration;
-    }
-
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public void consume() {
-        this.duration--;
     }
 
     @Override
