152bd657cccf491f2d021e6030668b800b17c00b
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-23 15:48:58 +0100

    Implemented ExtraHeart

--- src/main/java/javawulf/model/ExtraHeart.java
+++ src/main/java/javawulf/model/ExtraHeart.java
@@ -4,13 +4,11 @@ public class ExtraHeart extends Collectable implements Item {
 
     public ExtraHeart(PositionOnMap position, Integer points) {
         super(position, points);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void applyEffect(Player p) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'applyEffect'");
+        p.getPlayerHealth().increaseMaxHealth(1);
     }
 
 }
