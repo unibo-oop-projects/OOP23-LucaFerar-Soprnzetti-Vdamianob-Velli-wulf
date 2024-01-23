d2bdc9990d356c8a1d5c1a4709c97783b85d7c98
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-23 16:36:09 +0100

    implemented durability

--- src/main/java/javawulf/model/SwordImpl.java
+++ src/main/java/javawulf/model/SwordImpl.java
@@ -7,17 +7,28 @@ public class SwordImpl extends GameObject implements Sword {
     private final static int NORMAL = 1;
     private final static int STRONG = 2;
     private int strength;
+    private int durability;
     private SwordType swordType;
 
     public SwordImpl(PositionOnMap position){
         super(position, new BoundingBoxImpl(0, 0, 0, 0, CollisionType.SWORD));
         this.strength = NORMAL;
-        this.swordType = SwordType.GREATSWORD;
+        this.swordType = SwordType.NORMAL;
     }
 
     @Override
     public int getSwordStrength() {
         return this.strength;
+    }
+
+    @Override
+    public int getDurability() {
+        return this.durability;
+    }
+
+    @Override
+    public void setDurability(int durability) {
+        this.durability = durability;
     }
 
     @Override
@@ -36,6 +47,18 @@ public class SwordImpl extends GameObject implements Sword {
     public void changeSwordType() {
         this.swordType = this.swordType==SwordType.NORMAL ? SwordType.GREATSWORD : SwordType.NORMAL;
         this.strength = this.swordType==SwordType.GREATSWORD ? STRONG : this.strength;
+        if (this.swordType==SwordType.GREATSWORD){
+            this.setDurability(50);
+        }
     }
+
+    @Override
+    public void consume() {
+        this.durability--;
+    }
+
+    
+
+    
     
 }
