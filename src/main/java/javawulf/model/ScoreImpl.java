699ec6b82fe24a2b51e78b4c956e9e4d89c1f918
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-23 12:03:39 +0100

    First implementation of Score

---
+++ src/main/java/javawulf/model/ScoreImpl.java
@@ -0,0 +1,33 @@
+package javawulf.model;
+
+public class ScoreImpl implements Score{
+
+    private int points;
+    private Multiplier multiplier;
+
+    public ScoreImpl(){
+        this.points = 0;
+        this.multiplier = Multiplier.DEFAULT;
+    }
+
+    @Override
+    public int getPoints() {
+        return this.points;
+    }
+
+    @Override
+    public int getMultiplier() {
+        return this.multiplier.getValue();
+    }
+
+    @Override
+    public void setMultiplier(Multiplier multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    @Override
+    public void addPoints(int points) {
+        this.points = this.points + points*this.getMultiplier();
+    }
+    
+}
