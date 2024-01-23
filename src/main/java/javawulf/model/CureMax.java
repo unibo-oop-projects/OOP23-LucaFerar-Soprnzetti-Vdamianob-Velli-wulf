2286237a4c9384471d871baeb68aa7f0de9ae93b
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-23 16:07:24 +0100

    Implemented CureMax

--- src/main/java/javawulf/model/CureMax.java
+++ src/main/java/javawulf/model/CureMax.java
@@ -4,13 +4,11 @@ public class CureMax extends Cure {
 
     public CureMax(PositionOnMap position, Integer points) {
         super(position, points);
-        //TODO Auto-generated constructor stub
     }
 
     @Override
     public void applyEffect(Player p) {
-        // TODO Auto-generated method stub
-        super.applyEffect(p);
+        p.getPlayerHealth().setHealth(p.getPlayerHealth().getMaxHealth());
     }
     
 }
