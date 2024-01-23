ba9a036cfa9a2d76208407ad4cb1b6dcad6edeb2
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-23 19:28:29 +0100

    Added jUnit test for Biomes

--- src/test/java/javawulf/MapTest.java
+++ src/test/java/javawulf/MapTest.java
@@ -3,11 +3,15 @@ package javawulf;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import javafx.util.Pair;
+import javawulf.model.map.Biome;
+import javawulf.model.map.BiomeImpl;
 import javawulf.model.map.Corridor;
 import javawulf.model.map.Room;
 import javawulf.model.map.Space;
 import javawulf.model.map.Tile;
 import javawulf.model.map.TileImpl;
+import javawulf.model.map.TilePosition;
 import javawulf.model.map.TileType;
 
 public class MapTest {
@@ -46,5 +50,17 @@ public class MapTest {
         Space corridor2 = new Corridor(6, 3);
         assertEquals(6, corridor2.getWidth());
         assertEquals(3, corridor2.getHeight());
+    }
+
+    @Test
+    void testBiomes() {
+        Biome firstBiome = new BiomeImpl();
+        firstBiome.addRoom(new TilePosition(5, 5), new Room(10, 10));
+        firstBiome.addRoom(new TilePosition(10, 5), new Room(7, 6));
+        
+        assertEquals(2, firstBiome.getRooms().size());
+        assertEquals(0, firstBiome.getCorridors().size());
+
+        assertThrows(IllegalArgumentException.class, () -> firstBiome.addRoom(new TilePosition(200, 500), new Room(5, 4)));
     }
 }
