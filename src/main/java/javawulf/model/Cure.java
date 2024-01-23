f62f1bb132607c08d7100e526959e18df2ac6b2a
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-23 15:53:52 +0100

    Implemented Cure

--- src/main/java/javawulf/model/Cure.java
+++ src/main/java/javawulf/model/Cure.java
@@ -4,13 +4,11 @@ public class Cure extends Collectable implements Item {
 
     public Cure(PositionOnMap position, Integer points) {
         super(position, points);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void applyEffect(Player p) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'applyEffect'");
+        p.getPlayerHealth().setHealth(1);
     }
 
 }
