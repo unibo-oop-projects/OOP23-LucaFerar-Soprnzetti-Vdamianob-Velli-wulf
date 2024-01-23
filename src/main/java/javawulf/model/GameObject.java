93a9b24826436abcc76bf30091ce2064382f22c5
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-23 12:23:10 +0100

    Added constructor in GameObject

--- src/main/java/javawulf/model/GameObject.java
+++ src/main/java/javawulf/model/GameObject.java
@@ -1,9 +1,24 @@
 package javawulf.model;
 
+import javawulf.model.BoundingBox.CollisionType;
+
 public abstract class GameObject implements GameElement{
 
     private BoundingBox collision;
     private PositionOnMap position;
+
+    public GameObject(PositionOnMap position, BoundingBox collision){
+        this.position = position;
+        this.collision = collision;
+    }
+
+    public GameObject(PositionOnMap position, CollisionType type){
+        //the size of the element should be factor, likely through a parameter of the constructor
+        //or as a private static final variable (as it could be the standard one),
+        //otherwise it would be difficult to calculate the area surronding the position of the element
+        //which in this case would be the standard one (player/enemy/normal sword/collectables) 
+        this(position, new BoundingBoxImpl(0, 0, 0, 0, type));
+    }
 
     public BoundingBox getBounds(){
         return this.collision;
