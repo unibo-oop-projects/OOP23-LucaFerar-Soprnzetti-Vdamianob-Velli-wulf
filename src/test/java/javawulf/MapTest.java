db77c23a2354f0677a177cf494dc9691ff415313
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-23 12:33:05 +0100

    Added a simple test for Tile

--- src/test/java/javawulf/MapTest.java
+++ src/test/java/javawulf/MapTest.java
@@ -1,10 +1,17 @@
 package javawulf;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 
+import javawulf.model.map.Tile;
+import javawulf.model.map.TileType;
+
 public class MapTest {
     @Test void testTiles() {
-        
+        Tile firstTile = new TileImpl(TileType.CORRIDOR);
+        assertEquals(TileType.CORRIDOR, firstTile.getType());
+        firstTile.setType(TileType.ROOM);
+        assertEquals(TileType.ROOM, firstTile.getType());
     }
 }
