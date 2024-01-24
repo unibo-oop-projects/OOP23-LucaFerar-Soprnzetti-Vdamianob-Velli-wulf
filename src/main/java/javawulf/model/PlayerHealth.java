82304eb6a522a93ea9faeb29356c1c056049c9fa
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-24 11:03:31 +0100

    Made javadoc for both PlayerHealth and Sword interfaces

--- src/main/java/javawulf/model/PlayerHealth.java
+++ src/main/java/javawulf/model/PlayerHealth.java
@@ -1,6 +1,14 @@
 package javawulf.model;
 
+/**
+ *  PlayerHealth is a class which manages all that is related to the player's health
+ *  including its loss (by damage) and its recovery (by obtaining certain items)
+ */
 public interface PlayerHealth {
+    
+    /**
+     * ShieldStatus defines the current status of the player's shield. By default it is NONE
+     */
     public enum ShieldStatus{
         NONE(0),
         HALF(1),
@@ -17,16 +25,40 @@ public interface PlayerHealth {
         }
     }
 
+    /**
+     * @return The current health the player has
+     */
     public int getHealth();
 
+    /**
+     * @return The maximum health the player can currently have
+     */
     public int getMaxHealth();
 
+    /**
+     * @return the current shield status 
+     */
     public ShieldStatus getShieldStatus();
 
+    /**
+     * Update the player's health by adding/removing a determined amount.
+     * If it is greater than the player's maxHealth then it will be
+     * equal to maxHealth
+     * 
+     * @param health The variation the player's health
+     */
     public void setHealth(int health);
 
+    /**
+     * Increase the maximum amount of health the player can have at once
+     * 
+     * @param increase The amount by which the maximum health of player has to increase 
+     */
     public void increaseMaxHealth(int increase);
 
+    /**
+     * @param status The status the shield must now have 
+     */
     public void setShieldStatus(ShieldStatus status);
     
 }
--- src/main/java/javawulf/model/Sword.java
+++ src/main/java/javawulf/model/Sword.java
@@ -1,22 +1,44 @@
 package javawulf.model;
 
+/**
+ * Sword is class whose purpose is to deal with all of the attack functions of player,
+ * which are all linked to the player character's sword and its statistics
+ */
 public interface Sword extends GameElement {
     
+    /**
+     * SwordType defines the current type of sword. By default is is NORMAL
+     */
     public enum SwordType{
         NORMAL,
         GREATSWORD;
     }
-     
+    
+    /**
+     * @return The current strength of the player's sword
+     */
     public int getSwordStrength();
 
     public int getDurability();
 
     public void setDurability(int durability);
 
+    /**
+     * Set the sword strength. It is used both when the sword type is GREATSWORD
+     * and when a strength boosting power-up is obtained
+     * 
+     * @param strength The strength the sword changes into
+     */
     public void setSwordStrength(int strength);
 
+    /**
+     * @return The current type of the player's sword
+     */
     public SwordType getSwordType();
 
+    /**
+     * Changes the sword's type automatically
+     */
     public void changeSwordType();
 
     /**
