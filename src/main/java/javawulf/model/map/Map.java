ec94d874b3bd40a2a3f987d947df0891fdac96d6
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-24 12:54:40 +0100

    Little fix (formatted document)

--- src/main/java/javawulf/model/map/Map.java
+++ src/main/java/javawulf/model/map/Map.java
@@ -3,7 +3,8 @@ package javawulf.model.map;
 import javawulf.model.PositionOnMap;
 
 /**
- * Map is the complete bidimensional game Enviroment. It takes four biomes and it uses their to build the enviroment.
+ * Map is the complete bidimensional game Enviroment. It takes four biomes and
+ * it uses their to build the enviroment.
  * Enviroment will be:
  * <img src="../../../../resources/javadoc/biome-map-suddivision.png" />
  */
@@ -14,6 +15,7 @@ public interface Map {
      * @return tile position
      */
     TilePosition getTilePosition(PositionOnMap position);
+
     /**
      * 
      * @param position (absolute)
