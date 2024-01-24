713382954dffbd1220ce92e28196218c6505e069
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-24 22:34:06 +0100

    Updated Direction so that it has two fields representing how much the movement delta has to be multiplied by in order for an Entity to move in that direction

--- src/main/java/javawulf/model/Direction.java
+++ src/main/java/javawulf/model/Direction.java
@@ -1,12 +1,28 @@
 package javawulf.model;
 
 public enum Direction {
-    UP, 
-    DOWN, 
-    LEFT, 
-    RIGHT, 
-    UP_LEFT, 
-    UP_RIGHT, 
-    DOWN_LEFT, 
-    DOWN_RIGHT
+    UP(0.0,-1.0),
+    DOWN(0.0,1.0),
+    LEFT(-1.0,0.0),
+    RIGHT(1.0,0.0),
+    UP_LEFT(-Math.cos(Math.PI/4), -Math.sin(Math.PI/4)),
+    UP_RIGHT(Math.cos(Math.PI/4), -Math.sin(Math.PI/4)), 
+    DOWN_LEFT(-Math.cos(Math.PI/4), Math.sin(Math.PI/4)), 
+    DOWN_RIGHT(Math.cos(Math.PI/4), Math.sin(Math.PI/4));
+
+    private double x;
+    private double y;
+
+    Direction(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
 }
