4dc7e35e63b0af0dfbbfa4bfc4c5513823bc7b9f
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-25 12:03:00 +0100

    Starting implementing Map class (added impl. of Constructor)

--- src/main/java/javawulf/model/map/MapImpl.java
+++ src/main/java/javawulf/model/map/MapImpl.java
@@ -13,9 +13,10 @@ public class MapImpl implements Map {
     private static int WIDTH_CENTRAL_BIOME = 12;
     private final int MAP_SIZE = Biome.SIZE * 2 + WIDTH_CENTRAL_BIOME;
     private final List<Biome> biomes = new ArrayList<>();
-    private final java.util.Map<TilePosition, Tile> tileEnviroment = new HashMap<>();
+    private final java.util.Map<TilePosition, Tile> tiles = new HashMap<>();
 
     public MapImpl(Biome firstBiome, Biome secondBiome, Biome thirdBiome, Biome fourthBiome) {
+        this.biomes.addAll(List.of(firstBiome, secondBiome, thirdBiome, fourthBiome));
         build();
     }
 
