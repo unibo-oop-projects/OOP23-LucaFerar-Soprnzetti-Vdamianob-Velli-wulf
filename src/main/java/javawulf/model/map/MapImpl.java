00f6fb9240811f4f32e309b4bb1db91c1294a387
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-25 13:11:37 +0100

    Implemented build map function about rooms

--- src/main/java/javawulf/model/map/MapImpl.java
+++ src/main/java/javawulf/model/map/MapImpl.java
@@ -13,7 +13,30 @@ public class MapImpl implements Map {
     private static int WIDTH_CENTRAL_BIOME = 12;
     private final int MAP_SIZE = Biome.SIZE * 2 + WIDTH_CENTRAL_BIOME;
     private final List<Biome> biomes = new ArrayList<>();
-    private final java.util.Map<TilePosition, Tile> tiles = new HashMap<>();
+    private final java.util.Map<TilePosition, TileType> tiles = new HashMap<>();
+
+    enum BiomeOffset {
+        FIRST(0, new TilePosition(0, 0)), // TODO: defines offsets!
+        SECOND(1, new TilePosition(Biome.SIZE, 0)),
+        THIRD(2, new TilePosition(2 * Biome.SIZE, 0)),
+        FOURTH(3, new TilePosition(3 * Biome.SIZE, 0));
+
+        private final int pos;
+        private final TilePosition offset;
+
+        BiomeOffset(int pos, TilePosition offset) {
+            this.pos = pos;
+            this.offset = offset;
+        }
+
+        public TilePosition getOffset() {
+            return this.offset;
+        }
+
+        public int getPos() {
+            return this.pos;
+        }
+    }
 
     public MapImpl(Biome firstBiome, Biome secondBiome, Biome thirdBiome, Biome fourthBiome) {
         this.biomes.addAll(List.of(firstBiome, secondBiome, thirdBiome, fourthBiome));
@@ -33,7 +56,16 @@ public class MapImpl implements Map {
     }
 
     private void build() {
-        
+        for (var biomeOffSet : BiomeOffset.values()) {
+            for (var room : biomes.get(biomeOffSet.getPos()).getRooms()) {
+                for(int y = room.getKey().getY(); y < room.getValue().getHeight(); y++) {
+                    for(int x = room.getKey().getX(); x < room.getValue().getWidth(); x++) {
+                        this.tiles.put(new TilePosition(x+biomeOffSet.getOffset().getX(), y+biomeOffSet.getOffset().getY()), Room.defaultType);
+                    }
+                }
+
+            }
+        }
     }
 
 }
