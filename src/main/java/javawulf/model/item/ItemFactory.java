23774cb259344aa2ce180bb7266c7e9621637552
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-26 15:48:32 +0100

    Created a factory for the Items

---
+++ src/main/java/javawulf/model/item/ItemFactory.java
@@ -0,0 +1,66 @@
+package javawulf.model.item;
+
+import javawulf.model.Coordinate;
+
+public class ItemFactory {
+    public enum ItemType {
+        AMULET_FRAGMENTS("Amulet Fragments") {
+            @Override
+            public Item create(Coordinate position, Integer points) {
+                return new AmuletFragments(position, points);
+            }
+        },
+        GREAT_SWORD("Great Sword") {
+            @Override
+            public Item create(Coordinate position, Integer points) {
+                return new GreatSword(position, points);
+            }
+        },
+        CURE("Cure") {
+            @Override
+            public Item create(Coordinate position, Integer points) {
+                return new Cure(position, points);
+            }
+        },
+        CURE_MAX("Cure Max") {
+            @Override
+            public Item create(Coordinate position, Integer points) {
+                return new CureMax(position, points);
+            }
+        },
+        EXTRA_HEART("Extra Heart") {
+            @Override
+            public Item create(Coordinate position, Integer points) {
+                return new ExtraHeart(position, points);
+            }
+        },
+        MINIMAP("Minimap") {
+            @Override
+            public Item create(Coordinate position, Integer points) {
+                return new Minimap(position, points);
+            }
+        },
+        SHIELD("Shield") {
+            @Override
+            public Item create(Coordinate position, Integer points) {
+                return new Shield(position, points);
+            }
+        };
+
+        private final String name;
+
+        ItemType(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public abstract Item create(Coordinate position, Integer points);
+    }
+
+    public Item orderItem(ItemType type, Coordinate position, Integer points) {
+        return type.create(position, points);
+    }
+}
