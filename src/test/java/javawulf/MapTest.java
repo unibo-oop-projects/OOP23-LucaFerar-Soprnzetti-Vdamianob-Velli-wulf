d66659604e8579447aeda3a223b39165369ea11b
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-26 17:54:53 +0100

    Small fix (deleted useless import in MapTest)

--- src/test/java/javawulf/MapTest.java
+++ src/test/java/javawulf/MapTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Optional;
 import java.util.Set;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javawulf.model.BoundingBox;
