1b256eaca5d727390edb5bcc17f327526a054194
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-29 21:31:08 +0100

    Improving first #1 default map in Map Factory (to be finished!)

--- src/main/java/javawulf/model/map/MapFactoryImpl.java
+++ src/main/java/javawulf/model/map/MapFactoryImpl.java
@@ -8,12 +8,12 @@ public class MapFactoryImpl implements MapFactory{
     @Override
     public Map getDefaultMap1() {
         this.firstBiome = new BiomeImpl();
-        // firstBiome.addRoom(new TilePosition(0, 0), new Room(10, 10));
         firstBiome.addRoom(new TilePosition(2, 2), new RoomFactoryImpl().getSquaredRoom());
         firstBiome.addRoom(new TilePosition(9, 3), new RoomFactoryImpl().getSquaredRoom());
         firstBiome.addRoom(new TilePosition(8, 11), new RoomFactoryImpl().getSquaredRoom());
         firstBiome.addCorridor(new TilePosition(7, 4), new Corridor(2, 2));
         firstBiome.addCorridor(new TilePosition(10, 8), new Corridor(2, 3));
+
 
         this.secondBiome = new BiomeImpl();
         secondBiome.addRoom(new TilePosition(2, 2), new RoomFactoryImpl().getSquaredRoom());
@@ -28,16 +28,29 @@ public class MapFactoryImpl implements MapFactory{
         secondBiome.addCorridor(new TilePosition(3, 7), new Corridor(2, 6));
 
         this.thirdBiome = new BiomeImpl();
-        thirdBiome.addRoom(new TilePosition(0, 0), new Room(10, 10));
-        // thirdBiome.addRoom(new TilePosition(10, 6), new RoomFactoryImpl().getSquaredRoom());
-        // thirdBiome.addRoom(new TilePosition(35, 35), new RoomFactoryImpl().getSquaredRoom());
-
+        thirdBiome.addRoom(new TilePosition(2, 2), new RoomFactoryImpl().getSquaredRoom());
+        thirdBiome.addRoom(new TilePosition(13, 2), new RoomFactoryImpl().getSquaredRoom());
+        thirdBiome.addRoom(new TilePosition(12, 14), new RoomFactoryImpl().getSquaredRoom());
+        thirdBiome.addRoom(new TilePosition(1, 13), new RoomFactoryImpl().getSquaredRoom());
+        thirdBiome.addCorridor(new TilePosition(7, 4), new Corridor(6, 2));
+        thirdBiome.addCorridor(new TilePosition(10, 6), new Corridor(2, 6));
+        thirdBiome.addCorridor(new TilePosition(14, 10), new Corridor(2, 4));
+        thirdBiome.addCorridor(new TilePosition(14, 10), new Corridor(2, 4));
+        thirdBiome.addCorridor(new TilePosition(12, 10), new Corridor(2, 2));
+        thirdBiome.addCorridor(new TilePosition(3, 7), new Corridor(2, 6));
+        
         this.fourthBiome = new BiomeImpl();
-        // fourthBiome.addRoom(new TilePosition(0, 0), new Room(10, 10));
-        // fourthBiome.addRoom(new TilePosition(10, 6), new RoomFactoryImpl().getSquaredRoom());
-        // fourthBiome.addRoom(new TilePosition(35, 35), new RoomFactoryImpl().getSquaredRoom());
-        fourthBiome.addRoom(new TilePosition(3, 3), new Room(3, 3));
-        fourthBiome.addRoom(new TilePosition(6, 6), new Room(3, 3));
+        fourthBiome.addRoom(new TilePosition(2, 4), new RoomFactoryImpl().getSquaredRoom());
+        fourthBiome.addRoom(new TilePosition(13, 2), new RoomFactoryImpl().getSquaredRoom());
+        fourthBiome.addRoom(new TilePosition(12, 14), new RoomFactoryImpl().getSquaredRoom());
+        fourthBiome.addRoom(new TilePosition(1, 13), new RoomFactoryImpl().getSquaredRoom());
+        fourthBiome.addCorridor(new TilePosition(7, 5), new Corridor(5, 2));
+        fourthBiome.addCorridor(new TilePosition(10, 3), new Corridor(3, 2));
+        fourthBiome.addCorridor(new TilePosition(10, 6), new Corridor(2, 6));
+        fourthBiome.addCorridor(new TilePosition(14, 10), new Corridor(2, 4));
+        fourthBiome.addCorridor(new TilePosition(14, 10), new Corridor(2, 4));
+        fourthBiome.addCorridor(new TilePosition(12, 10), new Corridor(2, 2));
+        fourthBiome.addCorridor(new TilePosition(6, 15), new Corridor(6, 2));
 
         this.mapToGenerate = new MapImpl(firstBiome, secondBiome, thirdBiome, fourthBiome);
 
