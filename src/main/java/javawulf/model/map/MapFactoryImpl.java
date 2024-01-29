42ff21fb6e08b7e380d5cedce778398ce856a783
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-29 18:24:35 +0100

    Continuing defaultMap #1 method of MapFactory class...

--- src/main/java/javawulf/model/map/MapFactoryImpl.java
+++ src/main/java/javawulf/model/map/MapFactoryImpl.java
@@ -8,24 +8,29 @@ public class MapFactoryImpl implements MapFactory{
     @Override
     public Map defaultMap1() {
         this.firstBiome = new BiomeImpl();
-        firstBiome.addRoom(new TilePosition(5, 5), new Room(5, 5));
-        firstBiome.addRoom(new TilePosition(20, 5), new Room(5, 5));
-        firstBiome.addCorridor(new TilePosition(10, 6), new Corridor(3, 10));
+        // firstBiome.addRoom(new TilePosition(0, 0), new Room(10, 10));
+        firstBiome.addRoom(new TilePosition(2, 2), new Room(5, 5));
+        firstBiome.addRoom(new TilePosition(9, 3), new Room(5, 5));
+        firstBiome.addRoom(new TilePosition(8, 11), new Room(5, 5));
+        firstBiome.addCorridor(new TilePosition(7, 4), new Corridor(2, 2));
+        firstBiome.addCorridor(new TilePosition(10, 8), new Corridor(2, 3));
 
         this.secondBiome = new BiomeImpl();
-        secondBiome.addRoom(new TilePosition(3, 3), new Room(5, 5));
+        secondBiome.addRoom(new TilePosition(0, 0), new Room(10, 10));
         // secondBiome.addRoom(new TilePosition(10, 6), new Room(5, 5));
         // secondBiome.addRoom(new TilePosition(35, 35), new Room(5, 5));
 
         this.thirdBiome = new BiomeImpl();
-        thirdBiome.addRoom(new TilePosition(1, 1), new Room(10, 10));
+        thirdBiome.addRoom(new TilePosition(0, 0), new Room(10, 10));
         // thirdBiome.addRoom(new TilePosition(10, 6), new Room(5, 5));
         // thirdBiome.addRoom(new TilePosition(35, 35), new Room(5, 5));
 
         this.fourthBiome = new BiomeImpl();
-        fourthBiome.addRoom(new TilePosition(1, 1), new Room(10, 10));
+        // fourthBiome.addRoom(new TilePosition(0, 0), new Room(10, 10));
         // fourthBiome.addRoom(new TilePosition(10, 6), new Room(5, 5));
         // fourthBiome.addRoom(new TilePosition(35, 35), new Room(5, 5));
+        fourthBiome.addRoom(new TilePosition(3, 3), new Room(3, 3));
+        fourthBiome.addRoom(new TilePosition(6, 6), new Room(3, 3));
 
         this.mapToGenerate = new MapImpl(firstBiome, secondBiome, thirdBiome, fourthBiome);
 
