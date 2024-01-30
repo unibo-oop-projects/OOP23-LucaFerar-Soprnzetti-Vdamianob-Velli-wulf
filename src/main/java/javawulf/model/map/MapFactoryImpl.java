a2b80d2f61c4e85b6cbb754306afab47d9e024d4
vdamianob <vittorio.brasini@studio.unibo.it>
2024-01-30 11:03:22 +0100

    Finished first default map #1 method in Map Factory class

--- src/main/java/javawulf/model/map/MapFactoryImpl.java
+++ src/main/java/javawulf/model/map/MapFactoryImpl.java
@@ -32,11 +32,11 @@ public class MapFactoryImpl implements MapFactory{
         thirdBiome.addRoom(new TilePosition(13, 2), new RoomFactoryImpl().getSquaredRoom());
         thirdBiome.addRoom(new TilePosition(12, 14), new RoomFactoryImpl().getSquaredRoom());
         thirdBiome.addRoom(new TilePosition(1, 13), new RoomFactoryImpl().getSquaredRoom());
-        thirdBiome.addCorridor(new TilePosition(7, 4), new Corridor(6, 2));
-        thirdBiome.addCorridor(new TilePosition(10, 6), new Corridor(2, 6));
+        thirdBiome.addCorridor(new TilePosition(6, 15), new Corridor(2, 2));
+        thirdBiome.addCorridor(new TilePosition(8, 10), new Corridor(2, 7));
+        thirdBiome.addCorridor(new TilePosition(14, 7), new Corridor(2, 7));
         thirdBiome.addCorridor(new TilePosition(14, 10), new Corridor(2, 4));
-        thirdBiome.addCorridor(new TilePosition(14, 10), new Corridor(2, 4));
-        thirdBiome.addCorridor(new TilePosition(12, 10), new Corridor(2, 2));
+        thirdBiome.addCorridor(new TilePosition(10, 10), new Corridor(4, 2));
         thirdBiome.addCorridor(new TilePosition(3, 7), new Corridor(2, 6));
         
         this.fourthBiome = new BiomeImpl();
