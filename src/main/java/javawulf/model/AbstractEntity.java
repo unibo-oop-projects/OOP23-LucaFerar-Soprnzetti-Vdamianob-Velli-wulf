1b478fc955d5010a5c7c02a5ec96ba3a5eceaad5
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-30 23:37:31 +0100

    added javadoc and changed the parameters and methods in final

--- src/main/java/javawulf/model/AbstractEntity.java
+++ src/main/java/javawulf/model/AbstractEntity.java
@@ -4,6 +4,9 @@ import javawulf.model.BoundingBox.CollisionType;
 import javawulf.model.map.Map;
 import javawulf.model.map.TileType;
 
+/**
+ * Abstract implementation of the Entity interface.
+ */
 public abstract class AbstractEntity extends GameObject implements Entity {
 
     public static final int MOVEMENT_DELTA = OBJECT_SIZE / 8;
@@ -11,34 +14,63 @@ public abstract class AbstractEntity extends GameObject implements Entity {
     private Direction direction;
     private int stun;
 
+    /**
+     * Creates an Entity.
+     * 
+     * @param position the position of the entity
+     * @param type     the collision type of the entity
+     * @param speed    the speed of the entity
+     */
     public AbstractEntity(final Coordinate position, final CollisionType type, final int speed) {
         super(position, type);
         this.speed = speed;
         this.direction = Direction.DOWN;
     }
 
+    /**
+     * @return the speed of the entity.
+     */
     public final int getSpeed() {
         return this.speed;
     }
 
+    /**
+     * @return the direction of the entity.
+     */
     public final Direction getDirection() {
         return this.direction;
     }
 
-    public final void setSpeed(int speed) {
+    /**
+     * Sets the speed of the entity.
+     * 
+     * @param speed the new speed the entity will have
+     */
+    public final void setSpeed(final int speed) {
         this.speed = speed;
     }
 
-    public final void setDirection(Direction direction) {
+    /**
+     * Changes the direction of the entity.
+     * 
+     * @param direction the new direction the entity will have
+     */
+    public final void setDirection(final Direction direction) {
         this.direction = direction;
     }
 
-    protected final boolean isCollidingWithWall(Map m) {
+    /**
+     * Checks if the entity is colliding with a wall.
+     * 
+     * @param m the map which the entity is in
+     * @return true if the entity is colliding with a wall, false otherwise
+     */
+    protected final boolean isCollidingWithWall(final Map m) {
         var tile = m.getTileTypes(this.getBounds());
         return tile.contains(TileType.WALL);
     }
 
-    public boolean isHit(BoundingBox box) {
+    public boolean isHit(final BoundingBox box) {
         return this.getBounds().isCollidingWith(box.getCollisionArea())
                 && control(box);
     }
@@ -57,7 +89,7 @@ public abstract class AbstractEntity extends GameObject implements Entity {
 
     protected abstract CollisionType originalCollisonType();
 
-    public final void setStun(int stun) {
+    public final void setStun(final int stun) {
         this.stun = stun;
     }
 }
