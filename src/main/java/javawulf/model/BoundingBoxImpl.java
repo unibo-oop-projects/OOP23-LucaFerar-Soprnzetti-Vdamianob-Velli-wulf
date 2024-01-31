3d6bc455a4ca198e58a714f0a7066afdc662ae55
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-31 10:21:54 +0100

    Added documentation to BoundingBoxImpl

--- src/main/java/javawulf/model/BoundingBoxImpl.java
+++ src/main/java/javawulf/model/BoundingBoxImpl.java
@@ -3,11 +3,24 @@ package javawulf.model;
 import java.awt.Rectangle;
 import java.util.Optional;
 
+/**
+ * Implementation of BoudingBox that considers the center point
+ * in order to form the collision area around it.
+ */
 public final class BoundingBoxImpl implements BoundingBox {
 
     private Optional<Rectangle> area;
     private CollisionType type;
 
+    /**
+     * Creates a new BoundingBox.
+     * 
+     * @param x The x axis position of the GameElement
+     * @param y The y axis position of the GameElement
+     * @param width The width of the collision area
+     * @param height The height of the collision area
+     * @param type The type of collision area
+     */
     public BoundingBoxImpl(final int x, final int y, final int width, final int height, final CollisionType type) {
         this.area = Optional.ofNullable(new Rectangle(x - width / 2, y - height / 2, width, height));
         this.type = type;
