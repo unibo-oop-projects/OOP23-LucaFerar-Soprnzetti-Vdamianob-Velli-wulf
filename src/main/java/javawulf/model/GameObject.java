a10cde81e7b76af7f216c29b15d6fc83dbe7092d
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-01-31 15:27:38 +0100

    Made methods and parameters final, also added javadoc

--- src/main/java/javawulf/model/GameObject.java
+++ src/main/java/javawulf/model/GameObject.java
@@ -2,39 +2,53 @@ package javawulf.model;
 
 import javawulf.model.BoundingBox.CollisionType;
 
-public abstract class GameObject implements GameElement{
+/**
+ * Implementation of the GameElement interface.
+ */
+public abstract class GameObject implements GameElement {
 
     public static final int OBJECT_SIZE = 24;
     private BoundingBox collision;
     private Coordinate position;
 
-    public GameObject(Coordinate position, BoundingBox collision){
+    /**
+     * Creates a new GameObject with a custom BoundingBox.
+     * 
+     * @param position the position of the object
+     * @param collision the collision type of the object
+     */
+    public GameObject(final Coordinate position, final BoundingBox collision) {
         this.position = position;
         this.collision = collision;
     }
 
-    public GameObject(Coordinate position, CollisionType type){
+    /**
+     * Creates a new GameObject with a default BoundingBox.
+     * 
+     * @param position the position of the object
+     * @param type the collision type of the object
+     */
+    public GameObject(final Coordinate position, final CollisionType type) {
         this(position, new BoundingBoxImpl(position.getX(), position.getY(), OBJECT_SIZE, OBJECT_SIZE, type));
     }
 
-    public GameObject(BoundingBox collision, Coordinate position) {
-        this.collision = collision;
-        this.position = position;
-    }
-
-    public BoundingBox getBounds() {
+    @Override
+    public final BoundingBox getBounds() {
         return this.collision;
     }
 
-    public Coordinate getPosition() {
+    @Override
+    public final Coordinate getPosition() {
         return this.position;
     }
 
-    public void setBounds(BoundingBox b) {
+    @Override
+    public final void setBounds(BoundingBox b) {
         this.collision = b;
     }
 
-    public void setPosition(Coordinate p) {
+    @Override
+    public final void setPosition(Coordinate p) {
         this.position = p;
     }
 
