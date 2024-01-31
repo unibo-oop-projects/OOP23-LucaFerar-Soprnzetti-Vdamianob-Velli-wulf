b9f053e74878ee7ebec4f1c08ed3e395d52d6085
Filippo Velli <filippo.velli@studio.unibo.it>
2024-01-31 15:22:35 +0100

    Added documentation to GameElement

--- src/main/java/javawulf/model/GameElement.java
+++ src/main/java/javawulf/model/GameElement.java
@@ -1,12 +1,27 @@
 package javawulf.model;
 
+/**
+ * GameElement represents an element of the game.
+ */
 public interface GameElement {
 
+    /**
+     * @return The BoundingBox of the element
+     */
     BoundingBox getBounds();
 
+    /**
+     * @return The current Coordinate of the element
+     */
     Coordinate getPosition();
 
+    /**
+     * @param b The BoundingBox the entity must now have
+     */
     void setBounds(BoundingBox b);
 
+    /**
+     * @param p The Coordinate the entity must now have
+     */
     void setPosition(Coordinate p);
 }
