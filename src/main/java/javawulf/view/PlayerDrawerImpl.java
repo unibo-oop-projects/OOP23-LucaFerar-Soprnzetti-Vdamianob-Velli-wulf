8cb0cc04abb9ab5ae12ebfc1760fcce7d4b87765
Filippo Velli <filippo.velli@studio.unibo.it>
2024-02-01 11:27:02 +0100

    Updated PlayerDrawer and its implementation

--- src/main/java/javawulf/view/PlayerDrawerImpl.java
+++ src/main/java/javawulf/view/PlayerDrawerImpl.java
@@ -3,11 +3,10 @@ package javawulf.view;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.List;
 
 import javax.imageio.ImageIO;
 
-import javawulf.model.map.TileType;
+import javawulf.model.player.Player;
 
 public class PlayerDrawerImpl implements PlayerDrawer {
 
@@ -16,8 +15,10 @@ public class PlayerDrawerImpl implements PlayerDrawer {
     private BufferedImage playerLeft;
     private BufferedImage playerRight;
     private BufferedImage sword;
+    private Player player;
 
-    public PlayerDrawerImpl(){
+    public PlayerDrawerImpl(Player player) {
+        this.player = player;
         try {
             this.playerUp = ImageIO.read(getClass().getResourceAsStream(ImagePath.PLAYER_UP.getPath()));
             this.playerDown = ImageIO.read(getClass().getResourceAsStream(ImagePath.PLAYER_DOWN.getPath()));
@@ -30,19 +31,31 @@ public class PlayerDrawerImpl implements PlayerDrawer {
     }
 
     @Override
-    public void draw(Graphics2D graphics, int x, int y, List<Boolean> directions, boolean attack) {
+    public void draw(Graphics2D graphics) {
         BufferedImage img = this.sword;
-        if (directions.get(3)) {
-            img = this.playerLeft;
-        } else if (directions.get(4)) {
-            img = this.playerRight;
-        } else if (directions.get(1)) {
-            img = this.playerUp;
-        } else {
-            img = this.playerDown;
+        
+        switch (this.player.getDirection()){
+            case UP_RIGHT:
+            case RIGHT:
+            case DOWN_RIGHT:
+                img = this.playerRight; 
+                break;
+            case UP_LEFT:
+            case LEFT:
+            case DOWN_LEFT:
+                img = this.playerLeft;
+                break;
+            case UP:
+                img = this.playerUp;
+                break;
+            case DOWN:
+                img = this.playerDown;
+                break;
         }
-        graphics.drawImage(img, x * TileType.TILE_DIMENSION * GamePanel.scale,
-                        y * TileType.TILE_DIMENSION * GamePanel.scale, GamePanel.tileSize, GamePanel.tileSize, null);
+
+        graphics.drawImage(img, (int) this.player.getBounds().getCollisionArea().getWidth() * GamePanel.scale,
+            (int) this.player.getBounds().getCollisionArea().getWidth() * GamePanel.scale, GamePanel.tileSize,
+            GamePanel.tileSize, null);
     }
     
 }
