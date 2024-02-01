fb2b173b9de6e9ad69eff11854bc14ffcffdf0e2
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-02-01 17:13:55 +0100

    New implementation for the Pawn's test

--- src/test/java/javawulf/PawnTest.java
+++ src/test/java/javawulf/PawnTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javawulf.model.AbstractEntity;
+import javawulf.model.BoundingBox;
+import javawulf.model.BoundingBoxImpl;
 import javawulf.model.Coordinate;
 import javawulf.model.CoordinateImpl;
 import javawulf.model.Direction;
@@ -24,51 +27,67 @@ public final class PawnTest {
     private static final int STARTING_HEALTH = 3;
 
     EnemyFactory factory = new EnemyFactoryImpl();
-    Player player = new PlayerImpl(STARTING_X * 3, STARTING_Y, STARTING_HEALTH, 0);
+    Player player = new PlayerImpl(STARTING_X * 3, STARTING_Y * 3, STARTING_HEALTH, 0);
     Coordinate position = new CoordinateImpl(STARTING_X, STARTING_Y);
     Map map = new MapFactoryImpl().getTestMap(player);
     Pawn pawn;
 
     @BeforeEach
     void createPawn() {
-        pawn = factory.createPawn(position);
+        this.pawn = factory.createPawn(position);
     }
 
     @Test
     void testMove() throws InterruptedException {
-        // Test coordinates
+        // Test coordinates and BoundingBox
         Coordinate testPos = pawn.getPosition();
+        var testBounds = pawn.getBounds().getCollisionArea();
+        int delta = pawn.getSpeed() * AbstractEntity.MOVEMENT_DELTA;
+        // Test coordinates and boundingbox for expected position of the pawn after
+        // moving
+        Coordinate expectedCoordinate = new CoordinateImpl(
+                pawn.getPosition().getX() + (int) (pawn.getDirection().getX() * delta),
+                pawn.getPosition().getY() + (int) (pawn.getDirection().getY() * delta));
+        BoundingBox expectedBounds = new BoundingBoxImpl(
+                expectedCoordinate.getX(),
+                expectedCoordinate.getY(),
+                AbstractEntity.OBJECT_SIZE,
+                AbstractEntity.OBJECT_SIZE,
+                CollisionType.ENEMY);
         // Move the pawn
-        pawn.setDirection(Direction.DOWN);
         pawn.move(player, map);
         // Test if the pawn moved
         assertNotEquals(testPos, pawn.getPosition());
-
+        assertNotEquals(testBounds, pawn.getBounds().getCollisionArea());
+        // Test if the pawn has moved where he should have to
+        assertEquals(expectedCoordinate.getPosition(), pawn.getPosition().getPosition());
+        assertEquals(expectedBounds.getCollisionArea(), pawn.getBounds().getCollisionArea());
         // Position the player in the pawn's path
-        player.getPosition().setPosition(pawn.getPosition().getX() + (int) pawn.getDirection().getX(),
-                pawn.getPosition().getY() + (int) pawn.getDirection().getY());
-        // Taking the pawn's direction
-        Direction testDir = pawn.getDirection();
-        // Move the pawn
-        pawn.move(player, map);
-
+        player.setPosition(new CoordinateImpl(pawn.getPosition().getX(),
+                pawn.getPosition().getY()));
+        player.getBounds().setCollisionArea(player.getPosition().getX(), player.getPosition().getY(),
+                AbstractEntity.OBJECT_SIZE,
+                AbstractEntity.OBJECT_SIZE);
         // Test if the player is hit
         assertTrue(player.isHit(pawn.getBounds()));
 
-        // Test if the pawn changed direction
-        assertNotEquals(testDir, pawn.getDirection());
-
         // Move away the player
-        player.getPosition().setPosition(STARTING_X * 10, STARTING_Y);
+        player.setPosition(new CoordinateImpl(STARTING_X * 10, STARTING_Y * 10));
+        player.getBounds().setCollisionArea(player.getPosition().getX(), player.getPosition().getY(),
+                AbstractEntity.OBJECT_SIZE,
+                AbstractEntity.OBJECT_SIZE);
 
         // Put the pawn near a wall
         pawn.setPosition(testPos);
-        pawn.getPosition().setPosition(testPos.getX() + STARTING_X, testPos.getY());
-        pawn.setDirection(Direction.UP);
+        pawn.setPosition(new CoordinateImpl(STARTING_X, STARTING_Y * 6));
+        pawn.getBounds().setCollisionArea(pawn.getPosition().getX(), pawn.getPosition().getY(),
+                AbstractEntity.OBJECT_SIZE,
+                AbstractEntity.OBJECT_SIZE);
+        pawn.setDirection(Direction.DOWN);
 
         // Check if the pawn changes direction after colliding with a wall
         pawn.move(player, map);
-        assertNotEquals(Direction.UP, pawn.getDirection());
+        assertNotEquals(Direction.DOWN, pawn.getDirection());
 
     }
 
@@ -78,9 +97,8 @@ public final class PawnTest {
          * Put the player and the pawn near each other so that the player can hit the
          * pawn
          */
-        player.getPosition().setPosition(pawn.getPosition().getX() + STARTING_X * 2,
-                pawn.getPosition().getY());
-
+        this.player = new PlayerImpl(STARTING_X * 3, STARTING_Y, STARTING_HEALTH, 0);
+        player.move(Direction.LEFT);
         // Hit the pawn
         player.attack();
 
@@ -97,6 +115,31 @@ public final class PawnTest {
         // Check if the player got the points
         assertNotEquals(0, player.getScore().getPoints());
 
+    }
+
+    @Test
+    void testTick() {
+        // Memorizing the initial time to wait
+        int initialTime = pawn.getTimeToWait();
+        // Tick the pawn
+        pawn.tick();
+        // Test if the tick count is 1 or 0 depending on the time to wait
+        if (initialTime == 1) {
+            assertEquals(0, pawn.getTickCount());
+        } else {
+            assertEquals(1, pawn.getTickCount());
+        }
+        // Memorizing the initial time to wait and initial direction
+        initialTime = pawn.getTimeToWait();
+        Direction initialDirection = pawn.getDirection();
+        int counter = 0;
+        // Call the tick method until the pawn changes direction or the counter reaches 100
+        while (pawn.getDirection().equals(initialDirection) && counter < 100){
+            pawn.tick();
+            counter++;
+        }
+        // Test if the pawn changed direction
+        assertNotEquals(initialDirection, pawn.getDirection());
     }
 
 }
