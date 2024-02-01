c01b607c35cd80c24502f8e3ad6c49fdb83aa378
vdamianob <vittorio.brasini@studio.unibo.it>
2024-02-01 11:36:03 +0100

    Added javadocs (fixing checkstyle)

--- src/main/java/javawulf/controller/GameLoop.java
+++ src/main/java/javawulf/controller/GameLoop.java
@@ -2,7 +2,25 @@ package javawulf.controller;
 
 import javawulf.model.map.Map;
 
+/**
+ * GameLoop is an important Controller element: determines the game Time.
+ * Number of ticks per seconds are determined by FPS value.
+ * In each tick, GameLoop update the state of all game elements: objects, enemies and player.
+ * After updating the game element states, GameLoop ask View to redraw graphics.  
+ * 
+ */
 public interface GameLoop {
+    /**
+     * How many ticks per second the game state and the View representation will be refredhed.
+     */
+    int FPS = 60;
+    /**
+     * GameLoop thread will be start only after this method is invoked.
+     */
     void startGameLoopThread();
+    /**
+     * 
+     * @return game Map.
+     */
     Map getMap();
 }
