ff1a8ee53ea9d0e4ae8daf8c41a534083ec0089e
Filippo Velli <filippo.velli@studio.unibo.it>
2024-02-01 15:58:07 +0100

    Removed sysout prints

--- src/main/java/javawulf/view/CommandListener.java
+++ src/main/java/javawulf/view/CommandListener.java
@@ -31,23 +31,18 @@ public class CommandListener implements KeyListener {
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             this.up = true;
-            System.out.println("Pressed UP");
         }
         if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             this.down = true;
-            System.out.println("Pressed DOWN");
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             this.left = true;
-            System.out.println("Pressed LEFT");
         }
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             this.right = true;
-            System.out.println("Pressed RIGHT");
         }
         if (e.getKeyCode() == KeyEvent.VK_COMMA) {
             this.attack = true;
-            System.out.println("Pressed ATTACK");
         }
         communicateToController();
     }
@@ -56,23 +51,18 @@ public class CommandListener implements KeyListener {
     public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_UP) {
             this.up = false;
-            System.out.println("Released UP");
         }
         if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             this.down = false;
-            System.out.println("Released DOWN");
         }
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             this.left = false;
-            System.out.println("Released LEFT");
         }
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             this.right = false;
-            System.out.println("Released RIGHT");
         }
         if (e.getKeyCode() == KeyEvent.VK_COMMA) {
             this.attack = false;
-            System.out.println("Released ATTACK");
         }
         communicateToController();
     }
