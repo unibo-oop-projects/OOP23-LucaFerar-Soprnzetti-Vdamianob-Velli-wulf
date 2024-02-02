2095a4347d3815d88aa5b079a601a69f758b4f76
Filippo Velli <filippo.velli@studio.unibo.it>
2024-02-02 12:19:32 +0100

    Now Sword is drawn correctly and introduced private method to rotate the image (likely will be moved to another class)

--- src/main/java/javawulf/view/PlayerDrawerImpl.java
+++ src/main/java/javawulf/view/PlayerDrawerImpl.java
@@ -1,6 +1,8 @@
 package javawulf.view;
 
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -48,23 +50,28 @@ public final class PlayerDrawerImpl implements PlayerDrawer {
     @Override
     public void draw(final Graphics2D graphics) {
         BufferedImage img = this.sword;
+        BufferedImage imgSword = this.sword;
 
         switch (this.player.getDirection()) {
             case UP_RIGHT:
             case RIGHT:
             case DOWN_RIGHT:
-                img = this.playerRight; 
+                img = this.playerRight;
+                imgSword = rotateImage(imgSword, "right");
                 break;
             case UP_LEFT:
             case LEFT:
             case DOWN_LEFT:
                 img = this.playerLeft;
+                imgSword = rotateImage(imgSword, "left");
                 break;
             case UP:
                 img = this.playerUp;
+                imgSword = rotateImage(imgSword, "up");
                 break;
             default:
                 img = this.playerDown;
+                imgSword = rotateImage(imgSword, "down");
                 break;
         }
 
@@ -77,18 +84,37 @@ public final class PlayerDrawerImpl implements PlayerDrawer {
         if (this.playerSword.getBounds().getCollisionType().equals(CollisionType.SWORD)) {
             int width = (int) this.playerSword.getBounds().getCollisionArea().getWidth();
             int height = (int) this.playerSword.getBounds().getCollisionArea().getHeight();
-            BufferedImage imgSword = this.sword;
             if (this.playerSword.getSwordType().equals(SwordType.GREATSWORD)) {
                 imgSword = this.sword;
             }
 
-            int swordX = playerX - this.player.getPosition().getX() + this.player.getSword().getPosition().getX();
-            int swordY = playerY - this.player.getPosition().getY() + this.player.getSword().getPosition().getY();
-            graphics.drawImage(imgSword,
-                swordX,
-                swordY,
-                width * GamePanel.scale, height * GamePanel.scale, null);
+            int swordX = playerX - (this.player.getPosition().getX() - this.player.getSword().getPosition().getX())
+                * GamePanel.scale;
+            int swordY = playerY - (this.player.getPosition().getY() - this.player.getSword().getPosition().getY())
+                * GamePanel.scale;
+            graphics.drawImage(imgSword, swordX, swordY, width * GamePanel.scale, height * GamePanel.scale, null);
         }
+    }
+
+    private BufferedImage rotateImage(BufferedImage startingImage, String direction) {
+        AffineTransform tx = new AffineTransform();
+        AffineTransformOp op;
+        BufferedImage finalImage = startingImage;
+        double theta=0;
+
+        if (direction.equals("up")) {
+            theta = 0;
+        } else if (direction.equals("down")) {
+            theta = Math.PI;
+        } else if (direction.equals("right")) {
+            theta = Math.PI/2;
+        } else {
+            theta = -Math.PI/2;
+        }
+        tx.rotate(theta, finalImage.getWidth() / 2, finalImage.getHeight() / 2);
+        op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+
+        return op.filter(finalImage, null);
     }
 
 }
