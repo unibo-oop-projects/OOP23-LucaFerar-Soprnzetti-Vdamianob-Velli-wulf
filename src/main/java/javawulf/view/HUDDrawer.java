cf283796abc9752cf653fc1099a0b528f69119f6
Filippo Velli <filippo.velli@studio.unibo.it>
2024-02-02 21:54:05 +0100

    Now AbstractDrawer has been created and now both PlayerDrawer and HUDDrawer implement the Drawer interface

--- src/main/java/javawulf/view/HUDDrawer.java
+++ src/main/java/javawulf/view/HUDDrawer.java
@@ -1,14 +1,69 @@
 package javawulf.view;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javawulf.model.player.Player;
+import javawulf.model.player.Sword.SwordType;
 
 /**
  * Class used in GamePanel in order to draw the HUD.
  */
-public interface HUDDrawer {
+public final class HUDDrawer extends AbstractDrawer {
 
-    /**
-     * @param graphics where the HUD is drawn
-     */
-    void draw(Graphics2D graphics);
+    private GamePanel gamePanel;
+    private BufferedImage health;
+    private BufferedImage maxHealth;
+    private BufferedImage shield;
+    private BufferedImage sword;
+    private BufferedImage greatsword;
+    private Player player;
+    
+    public HUDDrawer(final Player player, final GamePanel gamePanel) {
+        super(gamePanel);
+        this.gamePanel = gamePanel;
+        this.player = player;
+        try {
+            this.shield = imageLoader(ImagePath.SHIELD);
+            this.health = imageLoader(ImagePath.HEALTH);
+            this.maxHealth = imageLoader(ImagePath.MAX_HEALTH);
+            this.sword = imageLoader(ImagePath.SWORD);
+            this.greatsword = imageLoader(ImagePath.GREATSWORD);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(final Graphics2D graphics) {
+        int x = this.gamePanel.getHeight() / 9 - Player.OBJECT_SIZE / 2;
+        int y = this.gamePanel.getHeight() / 9 - Player.OBJECT_SIZE / 2;
+
+        int max = this.player.getPlayerHealth().getMaxHealth();
+        int current = this.player.getPlayerHealth().getHealth();
+        int shield = this.player.getPlayerHealth().getShieldStatus().getStrength();
+        int sword = this.player.getSword().getSwordType() == SwordType.NORMAL ? 1 : 2;
+        BufferedImage img;
+        int i;
+        for (i = 0; i < max + shield; i++) {
+            if (max <= i) {
+                img = this.shield;
+            } else if (current > i + 1) {
+                img = this.health;
+            } else { 
+                img = this.maxHealth;
+            }
+            graphics.drawImage(img, x + img.getWidth() * GamePanel.scale * i,
+                    y, GamePanel.tileSize, GamePanel.tileSize, null);
+        }
+        if (sword == 1){
+            img = this.sword;
+        } else {
+            img = this.greatsword;
+        }
+        graphics.drawImage(img, x + img.getWidth() * GamePanel.scale * i, y, GamePanel.tileSize,
+            GamePanel.tileSize, null);
+    }
+    
 }
--- src/main/java/javawulf/view/PlayerDrawer.java
+++ src/main/java/javawulf/view/PlayerDrawer.java
@@ -1,15 +1,99 @@
 package javawulf.view;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javawulf.model.BoundingBox.CollisionType;
+import javawulf.model.player.Player;
+import javawulf.model.player.Sword;
+import javawulf.model.player.Sword.SwordType;
 
 /**
- * Class used in GamePanel in order to draw the Player character.
+ * Implementation of PlayerDrawer.
  */
-public interface PlayerDrawer {
+public final class PlayerDrawer extends AbstractDrawer {
+
+    private BufferedImage playerUp;
+    private BufferedImage playerDown;
+    private BufferedImage playerLeft;
+    private BufferedImage playerRight;
+    private BufferedImage sword;
+    private BufferedImage greatsword;
+    private Player player;
+    private Sword playerSword;
 
     /**
-     * @param graphics where the Player is drawn
+     * The Player coming from the Controller.
+     * 
+     * @param player The Player character that must be drawn
+     * @param gamePanel The panel that must be updated
      */
-    void draw(Graphics2D graphics);
+    public PlayerDrawer(final Player player, final GamePanel gamePanel) {
+        super(gamePanel);
+        this.player = player;
+        this.playerSword = this.player.getSword();
+        try {
+            this.playerUp = imageLoader(ImagePath.PLAYER_UP);
+            this.playerDown = imageLoader(ImagePath.PLAYER_DOWN);
+            this.playerLeft = imageLoader(ImagePath.PLAYER_LEFT);
+            this.playerRight = imageLoader(ImagePath.PLAYER_RIGHT);
+            this.sword = imageLoader(ImagePath.SWORD);
+            this.greatsword = imageLoader(ImagePath.GREATSWORD);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(final Graphics2D graphics) {
+        BufferedImage img = this.sword;
+        BufferedImage imgSword = this.sword;
+        String direction;
+
+        switch (this.player.getDirection()) {
+            case UP_RIGHT:
+            case RIGHT:
+            case DOWN_RIGHT:
+                img = this.playerRight;
+                direction = "right";
+                break;
+            case UP_LEFT:
+            case LEFT:
+            case DOWN_LEFT:
+                img = this.playerLeft;
+                direction = "left";
+                break;
+            case UP:
+                img = this.playerUp;
+                direction = "up";
+                break;
+            default:
+                img = this.playerDown;
+                direction = "down";
+                break;
+        }
+
+        int playerX = getPlayerX();
+        int playerY = getPlayerY();
+
+        graphics.drawImage(img, playerX, playerY,
+            GamePanel.tileSize, GamePanel.tileSize, null);
+
+        if (this.playerSword.getBounds().getCollisionType().equals(CollisionType.SWORD)) {
+            int width = (int) this.playerSword.getBounds().getCollisionArea().getWidth();
+            int height = (int) this.playerSword.getBounds().getCollisionArea().getHeight();
+            if (this.playerSword.getSwordType().equals(SwordType.GREATSWORD)) {
+                imgSword = this.greatsword;
+            }
+            imgSword = rotateImage(imgSword, direction);
+
+            int swordX = playerX - (int) (this.player.getBounds().getCollisionArea().getX()
+                - this.player.getSword().getBounds().getCollisionArea().getX()) * GamePanel.scale;
+            int swordY = playerY - (int) (this.player.getBounds().getCollisionArea().getY()
+                - this.player.getSword().getBounds().getCollisionArea().getY()) * GamePanel.scale;
+            graphics.drawImage(imgSword, swordX, swordY, width * GamePanel.scale, height * GamePanel.scale, null);
+        }
+    }
 
 }
