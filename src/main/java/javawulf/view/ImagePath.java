c6f0e6389f0b737d22195fcb5f429452a7892156
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-02-02 21:23:19 +0100

    Updated ImagePath

--- src/main/java/javawulf/view/ImagePath.java
+++ src/main/java/javawulf/view/ImagePath.java
@@ -1,27 +1,28 @@
 package javawulf.view;
 
 /**
- * Each element of this enum contains a relative path that can be used to reach an image of a specific *View* element.
+ * Each element of this enum contains a relative path that can be used to reach
+ * an image of a specific *View* element.
  * Images are stored in /resources directory
  */
 public enum ImagePath {
-    /**<img src="../../../resources/tiles/wall.png"> Wall tile.*/
+    /** <img src="../../../resources/tiles/wall.png"> Wall tile. */
     WALL_TILE("/tiles/wall.png"),
-    /**<img src="../../../resources/tiles/room.png"> Room tile.*/
+    /** <img src="../../../resources/tiles/room.png"> Room tile. */
     ROOM_TILE("/tiles/room.png"),
-    /**<img src="../../../resources/tiles/central_room.png"> Central room tile.*/
+    /** <img src="../../../resources/tiles/central_room.png"> Central room tile. */
     CENTRAL_ROOM_TILE("/tiles/central_room.png"),
-    /**<img src="../../../resources/tiles/corridor.png"> Corridor tile.*/
+    /** <img src="../../../resources/tiles/corridor.png"> Corridor tile. */
     CORRIDOR_TILE("/tiles/corridor.png"),
-    /**<img src="../../../resources/player/player_up.png"> Player up.*/
+    /** <img src="../../../resources/player/player_up.png"> Player up. */
     PLAYER_UP("/player/player_up.png"),
-    /**<img src="../../../resources/player/player_down.png"> Player down.*/
+    /** <img src="../../../resources/player/player_down.png"> Player down. */
     PLAYER_DOWN("/player/player_down.png"),
-    /**<img src="../../../resources/player/player_left.png"> Player left.*/
+    /** <img src="../../../resources/player/player_left.png"> Player left. */
     PLAYER_LEFT("/player/player_left.png"),
-    /**<img src="../../../resources/player/player_right.png"> Player right.*/
+    /** <img src="../../../resources/player/player_right.png"> Player right. */
     PLAYER_RIGHT("/player/player_right.png"),
-    /**<img src="../../../resources/player/player/sword.png"> Sword.*/
+    /** <img src="../../../resources/player/player/sword.png"> Sword. */
     SWORD("/player/sword.png"),
 
     GREATSWORD("/player/greatsword.png"),
@@ -30,8 +31,8 @@ public enum ImagePath {
 
     MAX_HEALTH("/player/recoverable_health.png"),
 
-    PLAYER_SHIELD("/player/shield.png"),
-
+    SHIELD("/player/shield.png"),
+    /** <img src="../../../resources/enemies/pawn/pawn_up.png"> Pawn up. */
     PAWN_UP("/enemies/pawn/pawn_up.png"),
     /** <img src="../../../resources/enemies/pawn/pawn_down.png"> Pawn down. */
     PAWN_DOWN("/enemies/pawn/pawn_down.png"),
@@ -48,7 +49,9 @@ public enum ImagePath {
     /** <img src="../../../resources/items/great_sword.png"> Greatsword. */
     GREAT_SWORD("/items/great_sword.png"),
     /** <img src="../../../resources/items/shield.png"> Shield. */
-    SHIELD("/items/shield.png");
+    SHIELD_ITEM("/items/shield.png"),
+    /** <img src="../../../resources/items/amulet_piece.png"> Amulet Piece. */
+    AMULET_PIECE("/items/amulet_piece.png");
 
     private final String path;
 
