6ed2774c4fa1ce7e1823b47c05dad22eb0e95cb7
Roberto Soprnzetti <roberto.sopranzetti@studio.unibo.it>
2024-02-02 18:13:19 +0100

    Implemented constructor in PawnDrawerImpl

---
+++ src/main/java/javawulf/view/PawnDrawerImpl.java
@@ -0,0 +1,48 @@
+package javawulf.view;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+
+import javawulf.model.enemy.Pawn;
+
+/**
+ * Implementation of the PawnDrawer interface.
+ */
+public final class PawnDrawerImpl implements PawnDrawer {
+
+    private BufferedImage pawnUp;
+    private BufferedImage pawnDown;
+    private BufferedImage pawnLeft;
+    private BufferedImage pawnRight;
+    private List<Pawn> pawns;
+    private GamePanel gamePanel;
+
+    /**
+     * The Pawns coming from the Controller.
+     * 
+     * @param pawns     The Pawns that must be drawn
+     * @param gamePanel The game panel that must be updated
+     */
+    public PawnDrawerImpl(final List<Pawn> pawns, final GamePanel gamePanel) {
+        this.pawns = pawns;
+        this.gamePanel = gamePanel;
+        try {
+            this.pawnUp = ImageIO.read(getClass().getResourceAsStream(ImagePath.PAWN_UP.getPath()));
+            this.pawnDown = ImageIO.read(getClass().getResourceAsStream(ImagePath.PAWN_DOWN.getPath()));
+            this.pawnLeft = ImageIO.read(getClass().getResourceAsStream(ImagePath.PAWN_LEFT.getPath()));
+            this.pawnRight = ImageIO.read(getClass().getResourceAsStream(ImagePath.PAWN_RIGHT.getPath()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void draw(Graphics2D graphics) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'draw'");
+    }
+
+}
