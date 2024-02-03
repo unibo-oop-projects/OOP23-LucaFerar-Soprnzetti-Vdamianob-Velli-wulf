a0e7107813f798cff410888586c7d77d3d9f9c22
Filippo Velli <filippo.velli@studio.unibo.it>
2024-02-03 15:56:00 +0100

    Made small style fixes

--- src/main/java/javawulf/model/CoordinateImpl.java
+++ src/main/java/javawulf/model/CoordinateImpl.java
@@ -2,10 +2,19 @@ package javawulf.model;
 
 import java.awt.Point;
 
+/**
+ * Implementation of Coordinate.
+ */
 public final class CoordinateImpl implements Coordinate {
 
     private Point position;
 
+    /**
+     * Creates a new coordinate.
+     * 
+     * @param x The position on the X-axis
+     * @param y the position on the Y-axis
+     */
     public CoordinateImpl(final int x, final int y) {
         this.position = new Point(x, y);
     }
