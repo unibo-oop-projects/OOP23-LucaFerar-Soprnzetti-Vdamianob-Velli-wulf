39e1bb2fa504a35ac8c1e82054d69e8256103534
LucaFerar <luca.ferartofan@studio.unibo.it>
2024-02-03 23:52:15 +0100

    fixed sizing problems with buttons

--- src/main/java/javawulf/view/gameMenu/GameMenuView.java
+++ src/main/java/javawulf/view/gameMenu/GameMenuView.java
@@ -2,12 +2,22 @@ package javawulf.view.gameMenu;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 public class GameMenuView extends JPanel {
+
+    private static int BORDERS = 100;
 
     public GameMenuView() throws InterruptedException {
         CreateMenuGUI();
@@ -18,18 +28,51 @@ public class GameMenuView extends JPanel {
     
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension( 600, 480));
-        frame.setLayout(new GridLayout(4,1));
+        frame.setLayout(new BorderLayout());
+
+        JPanel menu = new JPanel(new GridLayout(4,1));
+        menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
 
         JButton startButton = new JButton("PLAY");
         JButton leaderboardButton = new JButton("Leaderboard");
         JButton guideButton = new JButton("Guide");
         JButton exitButton = new JButton("Exit");
 
+        // To limit Max button sixing
+        Dimension maxButtonSize = new Dimension(600, 100);
+        startButton.setMaximumSize(maxButtonSize);
+        leaderboardButton.setMaximumSize(maxButtonSize);
+        guideButton.setMaximumSize(maxButtonSize);
+        exitButton.setMaximumSize(maxButtonSize);
 
-        frame.add(startButton);
-        frame.add(leaderboardButton);
-        frame.add(guideButton);
-        frame.add(exitButton);
+        exitButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int choice = JOptionPane.showConfirmDialog(frame,
+                        "Sure you want to quit?",
+                        "Confirm exit",
+                        JOptionPane.YES_NO_OPTION);
+
+                if (choice == JOptionPane.YES_OPTION) {
+                    System.exit(0);
+                }
+            }
+        });
+
+        menu.add(startButton);
+        menu.add(leaderboardButton);
+        menu.add(guideButton);
+        menu.add(exitButton);
+
+        frame.add(menu, BorderLayout.CENTER);
+
+        frame.add(Box.createVerticalStrut(BORDERS), BorderLayout.NORTH);
+        frame.add(Box.createVerticalStrut(BORDERS), BorderLayout.SOUTH);
+        frame.add(Box.createHorizontalStrut(BORDERS), BorderLayout.WEST);
+        frame.add(Box.createHorizontalStrut(BORDERS), BorderLayout.EAST);
+
+    
 
         frame.setVisible(true);
     }
