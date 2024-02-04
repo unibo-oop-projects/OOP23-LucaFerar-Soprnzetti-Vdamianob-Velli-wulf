ccf3e65a8189e7bdd92fc3238915daa77cfe4377
vdamianob <vittorio.brasini@studio.unibo.it>
2024-02-04 02:32:25 +0100

    Small fix (deleted useless import)

--- src/main/java/javawulf/model/map/factory/Populator.java
+++ src/main/java/javawulf/model/map/factory/Populator.java
@@ -2,7 +2,6 @@ package javawulf.model.map.factory;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.stream.Collectors;
 
 import javafx.util.Pair;
 import javawulf.model.Collectable;
