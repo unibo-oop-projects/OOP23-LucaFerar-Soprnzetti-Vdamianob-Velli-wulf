212323cba346a1e7d428cdf775d88365c145f6b2
vdamianob <vittorio.brasini@studio.unibo.it>
2024-02-05 15:37:24 +0100

    Refactor TilePosition (deleted Pair implementation)

--- src/main/java/javawulf/model/map/TilePosition.java
+++ src/main/java/javawulf/model/map/TilePosition.java
@@ -1,20 +1,21 @@
 package javawulf.model.map;
 
-import javafx.util.Pair;
-
 /**
  * TilePosition is a kind of coordinate used to individuate tiles inside a bidimensional space (Map)
  * For example: TilePosition (1,2)  of TILE_A it means that TILE_A is in the second column and the third row of my tile-matrix.
  */
-public final class TilePosition extends Pair<Integer, Integer> {
+public final class TilePosition {
 
+    private final int x;
+    private final int y;
     /**
      * 
      * @param tileX column position
      * @param tileY row position
      */
     public TilePosition(final Integer tileX, final Integer tileY) {
-        super(tileX, tileY);
+        this.x = tileX;
+        this.y = tileY;
     }
 
     /**
@@ -22,7 +23,7 @@ public final class TilePosition extends Pair<Integer, Integer> {
      * @return x tile position
      */
     public Integer getX() {
-        return this.getKey();
+        return this.x;
     }
 
     /**
@@ -30,12 +31,37 @@ public final class TilePosition extends Pair<Integer, Integer> {
      * @return y tile position
      */
     public Integer getY() {
-        return this.getValue();
+        return this.y;
     }
 
     @Override
     public String toString() {
         return "TilePosition [x:" + getX() + " y:" + getY() + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TilePosition other = (TilePosition) obj;
+        if (x != other.x)
+            return false;
+        if (y != other.y)
+            return false;
+        return true;
     }
 
 }
